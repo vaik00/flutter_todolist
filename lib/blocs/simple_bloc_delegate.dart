b93202aeec48a73c9de1e6af85b747d263afef3e
Vadim Khalimendik <vadim.h@cleveroad.com>
2019-09-10 23:35:03 +0300

    first commit

---
+++ lib/blocs/simple_bloc_delegate.dart
@@ -0,0 +1,22 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+import 'package:bloc/bloc.dart';
+
+// We can extend `BlocDelegate` and override `onTransition` and `onError`
+// in order to handle transitions and errors from all Blocs.
+class SimpleBlocDelegate extends BlocDelegate {
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
+    super.onError(bloc, error, stacktrace);
+    print(error);
+  }
+}
---
+++ lib/blocs/tab/tab_bloc.dart
@@ -0,0 +1,21 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter_app/blocs/tab/tab.dart';
+import 'package:flutter_app/models/models.dart';
+
+class TabBloc extends Bloc<TabEvent, AppTab> {
+  @override
+  AppTab get initialState => AppTab.todos;
+
+  @override
+  Stream<AppTab> mapEventToState(TabEvent event) async* {
+    if (event is UpdateTab) {
+      yield event.tab;
+    }
+  }
+}
